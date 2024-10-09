ad05635c608e755e3758f26fcf090c8229c1c552
Gianna McLeod <153853338+giannamcleod@users.noreply.github.com>
2024-10-09 15:45:40 -0500

    Update FrogGame.java

--- FrogGame.java
+++ FrogGame.java
@@ -1,29 +1,4 @@
-//////////////// FILE HEADER (INCLUDE IN EVERY FILE) //////////////////////////
-//
-// Title:    P05 Froggie game
-// Course:   CS 300 Fall 2023
-//
-// Author:   Gianna McLeod & Jason Antonellis
-// Email:    gmmcleod@wisc.edu & jantonellis@wisc.edu
-// Lecturer: Mouna Kacem
-//
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
-//
-// Partner Name:    Jason Antonellis
-// Partner Email:   jantonellis@wisc.edu
-// Partner Lecturer's Name: Mouna Kacem
-//
-// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
-//   _X_ Write-up states that pair programming is allowed for this assignment.
-//   _X_ We have both read and understand the course Pair Programming Policy.
-//   _X_ We have registered our team prior to the team registration deadline.
-//
-///////////////////////// ALWAYS CREDIT OUTSIDE HELP //////////////////////////
-//
-// Persons:         Went to office hours for help on the circling bug logic
-// Online Sources:  None
-//
-///////////////////////////////////////////////////////////////////////////////
+
 import java.io.File;
 import java.util.ArrayList;
 
